1a162de804a3167c6e7af1944a018beefde22ec9
chendiyu <cdy1998cdy@163.com>
2025-03-16 20:09:54 +0800

    1

--- scripts/cli/src/index.ts
+++ scripts/cli/src/index.ts
@@ -1,2 +1,40 @@
+import cac from 'cac';
+import * as execa from 'execa';
+
 console.log(123);
 console.log(123);
+
+const cli = cac('oci');
+const { execaCommand } = execa;
+
+cli
+  .command('prettier')
+  .usage('Batch execute project lint check.')
+  .option('--format', 'Format lint problem.')
+  .action(runLint);
+
+async function runLint({ format }: any) {
+  if (format) {
+    await execaCommand(`stylelint "**/*.{vue,css,less.scss}" --cache --fix`, {
+      stdio: 'inherit',
+    });
+    await execaCommand(`eslint . --cache --fix`, {
+      stdio: 'inherit',
+    });
+    await execaCommand(`prettier . --write --cache --log-level warn`, {
+      stdio: 'inherit',
+    });
+    return;
+  }
+  await Promise.all([
+    execaCommand(`eslint . --cache`, {
+      stdio: 'inherit',
+    }),
+    execaCommand(`prettier . --ignore-unknown --check --cache`, {
+      stdio: 'inherit',
+    }),
+    execaCommand(`stylelint "**/*.{vue,css,less.scss}" --cache`, {
+      stdio: 'inherit',
+    }),
+  ]);
+}
